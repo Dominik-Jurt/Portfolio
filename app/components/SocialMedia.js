77aca5bf78208834f5a8b2134d7e0ad99b770fa0
Dominik Jurt <dominik.jurt@gmx.ch>
2023-09-04 23:08:11 +0200

    fixed issues with social media width on reduced width

--- app/components/SocialMedia.js
+++ app/components/SocialMedia.js
@@ -7,7 +7,7 @@ import EmailIcon from '@mui/icons-material/Email';
 
 function SocialMedia() {
     return (
-        <div className='items-start mx-auto lg:ml-20 flex flex-col items-center border border-gray-800 border-8 min-w-2/5 w-9/12 lg:w-1/4 mt-20 p-6 lg:p-12 mb-4'>
+        <div className='items-start mx-auto lg:ml-20 flex flex-col items-center border border-gray-800 border-8 min-w-2/5 w-11/12 lg:w-1/4 mt-20 p-6 lg:p-12 mb-4'>
             <div className="text-3xl font-bold mb-4 text-gray-800">Social Media</div>
             <div className='flex space-x-4 mt-8'>
                 <div className='flex items-center border border-gray-800 border-4 p-2'>
