8ecb7bc841a401b366075280da65ac1cdf3ddf87
Dominik Jurt <dominik.jurt@gmx.ch>
2023-09-04 15:18:23 +0200

    fixed header menu issues

--- app/components/pageContainers/Header.js
+++ app/components/pageContainers/Header.js
@@ -29,7 +29,7 @@ function Header() {
     return (
         <header className="bg-gray-800 py-1 sticky top-0 z-50">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-3xl font-semibold whitespace-nowrap">Dominik Jurt</div>
+                <div className="text-white text-3xl font-semibold whitespace-nowrap ml-4">Dominik Jurt</div>
                 <nav className="flex justify-between items-center">
                     <div className="hidden lg:flex lg:mr-12 lg:h-full">
                         <Link href="/" className="text-xl text-white rounded-md hover:text-gray-300 hover:bg-gray-700 py-6 px-4 font-light">Home</Link>
@@ -57,29 +57,20 @@ function Header() {
                             onClick={toggleDropdown}
                             className="text-white hover:text-gray-300 p-2"
                         >
-                            <MenuIcon className="text-6xl"/>
+                            <MenuIcon className="text-6xl" />
                         </button>
                         {isDropdownOpen && (
-                            <div className="absolute top-full left-0 bg-gray-800 border border-gray-700 mt-1 py-2">
+                            <div className="absolute top-full right-0 bg-gray-800 border border-gray-700 mt-1 py-2">
                                 <Link href="/" className="block text-xl text-white hover:text-gray-300 py-2 px-4">Home</Link>
                                 <Link href="/about" className="block text-xl text-white hover:text-gray-300 py-2 px-4">About</Link>
                                 <Link href="/projects" className="block text-xl text-white hover:text-gray-300 py-2 px-4">Projects</Link>
                                 <Link href="/resume" className="block text-xl text-white hover:text-gray-300 py-2 px-4">CV</Link>
                                 <Link href="/contact" className="block text-xl text-white hover:text-gray-300 py-2 px-4">Contact</Link>
-                                <div className="flex space-x-2 text-white items-center ml-16">
-                                    <div className='text-m hover:text-gray-300 cursor-pointer' onClick={() => toggleLanguage(true)}>Eng
+                                {isEnglish ?
+                                    <div className='block text-xl text-white hover:text-gray-300 py-2 px-4 cursor-pointer' onClick={() => toggleLanguage(false)}>Switch to German
                                     </div>
-                                    <div className="w-6 min-w-full h-1 rounded-full flex">
-                                        <div className={leftToggleColor}></div>
-                                        <div className="bg-gray-100 w-6/12 rounded-full"></div>
-                                        <div className={rightToggleColor}></div>
-                                    </div>
-                                    <div
-                                        className="text-m hover:text-gray-300 cursor-pointer"
-                                        onClick={() => toggleLanguage(false)}
-                                    >
-                                        De</div>
-                                </div>
+                                    : <div className='block text-xl text-white hover:text-gray-300 py-2 px-4 cursor-pointer' onClick={() => toggleLanguage(true)}>Switch to English
+                                    </div>}
                             </div>
                         )}
                     </div>
